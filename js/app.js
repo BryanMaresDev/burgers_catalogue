aab0f28729bcc27d0d07adeb983e50690a105915
Bryan Mares <bryanmaresdev@outlook.com>
2022-10-27 10:56:14 -0500

    Añadimos filtros a nuestra pagina

--- js/app.js
+++ js/app.js
@@ -3,11 +3,50 @@ const template = document.getElementById('template').content
 
 const fragment = document.createDocumentFragment()
 
+const txtNombre = document.getElementById('inputNombre')
+const btnBuscar = document.getElementById('btnBuscar')
+const btnTodos = document.getElementById('btnTodos')
+
+const txtNombreAlgo = document.getElementById('inputAlgo')
+const btnTodosAlgo = document.getElementById('btnTodosAlgo')
+
 let hamburgers = []
 
 document.addEventListener('DOMContentLoaded', () => {
     getHamburgers()
 })
+
+btnBuscar.addEventListener('click', () => {
+    console.log('input', txtNombre.value)
+    if (txtNombre.value.trim().length === 0) {
+        alert('No has escrito nada D:')
+        return
+    }
+
+    const find = hamburgers.filter(dato => {
+        return dato.name === txtNombre.value
+    })
+
+    if (find.length > 0) {
+        printHamburgers(find)
+    }else{
+        alert('Hamburguesa no encontrada')
+    }
+
+    console.log('find', find)
+})
+
+btnTodos.addEventListener('click', () => { printHamburgers(hamburgers)})
+
+/*
+txtNombreAlgo.addEventListener('input', () => {
+    console.log(':D')
+    const encontrados = hamburgers.indexOf( (dato) => {
+        return dato.name === txtNombreAlgo.value
+    })
+    printHamburgers(encontrados)
+})
+*/
 
 const getHamburgers = () => {
     const options = {
@@ -23,13 +62,14 @@ const getHamburgers = () => {
         .then(response => {
             hamburgers = response
             console.log(hamburgers)
-            printHamburgers()
+            printHamburgers(hamburgers)
         })
         .catch(err => console.error(err))
 }
 
-const printHamburgers = () => {
-    hamburgers.forEach(hamburger => {
+const printHamburgers = (obj) => {
+    listHamburger.innerHTML = ''
+    obj.forEach(hamburger => {
         template.querySelector('h1').textContent = hamburger.name
         
         template.querySelectorAll('li')[0].textContent = hamburger.ingredients[0]
